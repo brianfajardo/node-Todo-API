9e4eb666b131e9aaafd9242984d485c953c7d7ae
Brian Fajardo <bmf5@hotmail.com>
2017-03-08 06:08:24 -0500

    add find script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -6,19 +6,19 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server.')
 
-    // db.collection('Todos')
-    //     .find({
-    //         _id: new ObjectId("58bfd5fda5f96a99bdce2b76") /* query Todos collection */
-    //     })
-    //     .toArray()
-    //     .then((result) => {
-    //         console.log('Todos:');
-    //         console.log(JSON.stringify(result, undefined, 2))
-    //     })
-    //     .catch((err) => {
-    //         console.log('Unable to fetch todos.')
-    //     })
-    
+    db.collection('Todos')
+        .find({
+            _id: new ObjectId("58bfd5fda5f96a99bdce2b76") /* query Todos collection */
+        })
+        .toArray()
+        .then((result) => {
+            console.log('Todos:');
+            console.log(JSON.stringify(result, undefined, 2))
+        })
+        .catch((err) => {
+            console.log('Unable to fetch todos.')
+        })
+
     db.collection('Users')
         .find({ name: 'Jen' })
         .toArray() /* .toArray() returns a Promise */
