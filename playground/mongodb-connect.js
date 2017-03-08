5b6e0e868c856affdfa7b5e3ec72a6d67214cec0
Brian Fajardo <bmf5@hotmail.com>
2017-03-08 04:57:13 -0500

    destructure ObjectID's

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb') // ES6 destructuring
 
 // .connect('PATH SYNTAX', CALLBACK)
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
@@ -15,7 +16,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         if (err) {
             return console.log('Unable to insert todo into collection "Users"')
         }
-        console.log(result.ops)
+        console.log(result.ops[0]._id.getTimestamp())
     })
 
     db.close();
