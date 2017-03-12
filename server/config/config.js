3b01972f63ab0bd0a22ce3a9111f866794e41497
Brian Fajardo <bmf5@hotmail.com>
2017-03-12 09:22:28 -0400

    Setup seperate test and development envs

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+// Dynamically setting the node environment
+
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    // TodoAppTest is another database, won't overwrite development DB
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 * No need for micro-management of the order of things (like MongoDB)
 */
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose
